f65deb88a630140ba1001729cd5b1e71be998f1c
Humdan <humdanq7270@yahoo.com>
2026-07-06 12:34:43 -0600

    Delete Python directory

--- Python/OptionalExercises/Unit1OptionalExcerciseREMASTERED.py
+++
@@ -1,6 +0,0 @@
-#Printing them in order may make the illusion of the car
-#Roof
-print ("           ----------\n   ______/__]     [__\___\n (o  |  -|       -|     o|\n `-()----------------()--'")
-
-       
-       
